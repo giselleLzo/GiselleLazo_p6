9e3ab99c3b66351700c2f9dc6be03cc87a410992
Giselle <gisellelsz@gmail.com>
2021-07-01 19:13:02 +0200

    Import body parser, mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://giselle:Iannick.Loic2@cluster0.fgo1j.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('conexion a mongo'))
+    .catch(() => console.log('conexion echoue'));
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({message: '#'});
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
