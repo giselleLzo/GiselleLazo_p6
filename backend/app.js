5da277eda5ce198068e116c5a187ba8ee99e0308
Giselle <gisellelsz@gmail.com>
2021-07-09 01:23:20 +0200

    Simplify file

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const saucesRoutes = require('./routes/sauces');
 
 const userRoutes = require('./routes/user');
 
@@ -24,41 +24,7 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
-
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing ({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'objet enregistre'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.put('api/sauces/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'objet modifie' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('api/sauces/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'objet supprime' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('api/sauces/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/sauces', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
-
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
