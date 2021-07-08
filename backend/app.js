4c03152fd7cf79fcb409bd5e3c1557d6b393d04a
Giselle <gisellelsz@gmail.com>
2021-07-09 00:34:02 +0200

    Add const thing, post

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Thing = require('./models/Thing');
+
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://giselle:Iannick.Loic2@cluster0.fgo1j.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -19,7 +21,25 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+
+app.post('/api/sauces', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing ({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'objet enregistre'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/sauces', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+});
 
 app.use('/api/auth', userRoutes);
 
