d94a2fd7e17b56abf2d27da9b7b81135cc749b4a
Giselle <gisellelsz@gmail.com>
2021-07-09 03:41:20 +0200

    Add multer put

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, saucesCtrl.createThing);
-router.put('/:id', auth, saucesCtrl.modifyThing); 
+router.put('/:id', auth, multer, saucesCtrl.modifyThing); 
 router.delete('/:id', auth, saucesCtrl.deleteThing); 
 router.get('/:id', auth, saucesCtrl.getOneThing);  
 router.get('/', auth, saucesCtrl.getAllThings);  
