bde32d570fbe70b23dc3c6e07c63ccf6f8cccacf
Giselle <gisellelsz@gmail.com>
2021-07-09 02:02:06 +0200

    Authentification

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
 
-router.post('/', saucesCtrl.createThing);
-router.put('/:id', saucesCtrl.modifyThing); 
-router.delete('/:id', saucesCtrl.deleteThing); 
-router.get('/:id', saucesCtrl.deleteThing);  
-router.get('/', saucesCtrl.deleteThing);  
+router.post('/', auth, saucesCtrl.createThing);
+router.put('/:id', auth, saucesCtrl.modifyThing); 
+router.delete('/:id', auth, saucesCtrl.deleteThing); 
+router.get('/:id', auth, saucesCtrl.getOneThing);  
+router.get('/', auth, saucesCtrl.getAllThings);  
 
 module.exports = router;
