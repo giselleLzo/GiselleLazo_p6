49beb935c47b8a0e1f8ce743226a93d9fbc0735e
Giselle <gisellelsz@gmail.com>
2021-07-16 21:01:40 +0200

    Add password validation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passwordSchema = require('../middleware/passwordVerify');
 const userCtrl = require('../controllers/user');
 
 //Envoyer les informations de l'utilisateur depuis le frontend
