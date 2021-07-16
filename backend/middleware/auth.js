36b13923bf4b24c157c0cc017f3054f412944cab
Giselle <gisellelsz@gmail.com>
2021-07-16 02:36:18 +0200

    Add env url

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,18 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 
 module.exports = (req, res, next) => { //Vérification du token
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'User ID non valable';
+        if(!token) {
+            return res.status(403).json({error: 'Accès interdit'});
+          }
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+        
+        if (req.body.userId && (req.body.userId !== decodedToken.userId) ) {
+            return res.status(403).json({error: 'Identifiant non valide'});
         } else {
+            console.log(req.body);
             next();
         }
     } catch (error) {
